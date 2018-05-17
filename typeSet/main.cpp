9b89d91bb12e7851c062d7f7ceaf49977b7d7c81
dormon <imilet@fit.vutbr.cz>
2018-05-18 01:45:45 +0200

    typeset

--- typeSet/main.cpp
+++ typeSet/main.cpp
@@ -11,18 +11,28 @@
 template<typename...>
 struct TypeSet{};
 
+/*
 template<typename>
 constexpr bool contain(){
   return false;
 }
 
 template<typename A,typename B,typename...C>
-constexpr bool contain(){
+constexpr bool contain<A,TypeSet<B,C...>>(){
   if constexpr(std::is_same_v<A,B>)
     return true;
   return contain<A,C...>();
 }
+*/
 
+
+template<typename>
+struct contain: std::false_type{};
+
+template<typename>
+struct contain: std::
+
+/*
 template<typename,typename>
 struct Insert;
 
@@ -40,17 +50,34 @@ template<typename A,typename B,typename...C>
 struct Union<A,TypeSet<B,C...>>{
   using type = typename Union<typename Insert<B,A>::type,TypeSet<C...>>::type;
 };
+*/
+/*
+template<typename,typename>
+struct Intersect{
+  using type = TypeSet<>;
+};
 
+template<typename A,typename B,typename...C>
+struct Intersect<A,TypeSet<B,C...>>{
+  using type = std::conditional_t
+    <
+      contain<B,A>(),
+      typename Union<typename Intersect<A,TypeSet<C...>>::type,TypeSet<B>>::type,
+                     typename Intersect<A,TypeSet<C...>>::type
+    >;
+};
+*/
 
 int main() { 
-  static_assert(contain<float,int,char,float,double>());
-  static_assert(!contain<float>());
-  static_assert(!contain<float,int>());
-  static_assert(std::is_same_v<Insert<float,TypeSet<int>>::type,TypeSet<int,float>>);
-  static_assert(std::is_same_v<Insert<float,TypeSet<float,int>>::type,TypeSet<float,int>>);
-  static_assert(std::is_same_v<Union<TypeSet<float>,TypeSet<double>>::type,TypeSet<float,double>>);
-  static_assert(std::is_same_v<Union<TypeSet<float,int>,TypeSet<double>>::type,TypeSet<float,int,double>>);
-  static_assert(std::is_same_v<Union<TypeSet<float>,TypeSet<double,char>>::type,TypeSet<float,double,char>>);
+  static_assert(contain<float,TypeSet<int,float,char>>());
+  //static_assert(contain<float,TypeSet<int,char,float,double>>());
+  //static_assert(!contain<float>());
+  //static_assert(!contain<float,TypeSet<int>>());
+  //static_assert(std::is_same_v<Insert<float,TypeSet<int>>::type,TypeSet<int,float>>);
+  //static_assert(std::is_same_v<Insert<float,TypeSet<float,int>>::type,TypeSet<float,int>>);
+  //static_assert(std::is_same_v<Union<TypeSet<float>,TypeSet<double>>::type,TypeSet<float,double>>);
+  //static_assert(std::is_same_v<Union<TypeSet<float,int>,TypeSet<double>>::type,TypeSet<float,int,double>>);
+  //static_assert(std::is_same_v<Union<TypeSet<float>,TypeSet<double,char>>::type,TypeSet<float,double,char>>);
   //static_assert(std::is_same_v<Union<TypeSet<float,int,char>,TypeSet<float,std::string,double>>::type,TypeSet<float,int,char,std::string,double>>);
   return 0; 
 }
