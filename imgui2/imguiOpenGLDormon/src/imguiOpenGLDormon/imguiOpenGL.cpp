e44aafc7ab94cb214837b2b49069730cea81a901
dormon <imilet@fit.vutbr.cz>
2018-07-08 17:27:45 +0200

    imgui2

--- imgui2/imguiOpenGLDormon/src/imguiOpenGLDormon/imguiOpenGL.cpp
+++ imgui2/imguiOpenGLDormon/src/imguiOpenGLDormon/imguiOpenGL.cpp
@@ -7,66 +7,25 @@
 
 IMGUI_IMPL_API bool     ImGui_ImplOpenGL3_Init(const char* glsl_version = "#version 150");
 IMGUI_IMPL_API void     ImGui_ImplOpenGL3_NewFrame();
-IMGUI_IMPL_API void     ImGui_ImplOpenGL3_RenderDrawData(ImDrawData* draw_data);
 
 // Called by Init/NewFrame/Shutdown
-IMGUI_IMPL_API bool     ImGui_ImplOpenGL3_CreateFontsTexture();
 IMGUI_IMPL_API void     ImGui_ImplOpenGL3_DestroyFontsTexture();
 IMGUI_IMPL_API bool     ImGui_ImplOpenGL3_CreateDeviceObjects();
 IMGUI_IMPL_API void     ImGui_ImplOpenGL3_DestroyDeviceObjects();
-
-
-
-// OpenGL3 Render function.
-// (this used to be set in io.RenderDrawListsFn and called by ImGui::Render(), but you can now call this directly from your main loop)
-// Note that this implementation is little overcomplicated because we are saving/setting up/restoring every OpenGL state explicitly, in order to be able to run within any OpenGL engine that doesn't do so. 
-void    ImGui_ImplOpenGL3_RenderDrawData(ImDrawData* draw_data)
-{
-}
-
-bool ImGui_ImplOpenGL3_CreateFontsTexture()
-{
-    // Build texture atlas
-    ImGuiIO& io = ImGui::GetIO();
-    unsigned char* pixels;
-    int width, height;
-    io.Fonts->GetTexDataAsRGBA32(&pixels, &width, &height);   // Load as RGBA 32-bits (75% of the memory is wasted, but default font is so small) because it is more likely to be compatible with user's existing shaders. If your ImTextureId represent a higher-level concept than just a GL texture id, consider calling GetTexDataAsAlpha8() instead to save on GPU memory.
-
-    // Upload texture to graphics system
-    GLint last_texture;
-    glGetIntegerv(GL_TEXTURE_BINDING_2D, &last_texture);
-    glGenTextures(1, &g_FontTexture);
-    glBindTexture(GL_TEXTURE_2D, g_FontTexture);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
-
-    // Store our identifier
-    io.Fonts->TexID = (void *)(intptr_t)g_FontTexture;
-
-    // Restore state
-    glBindTexture(GL_TEXTURE_2D, last_texture);
-
-    return true;
-}
 
 class ImguiOpenGLImpl{
   public:
     ImguiOpenGLImpl(char const*version);
     ~ImguiOpenGLImpl();
     void render(ImDrawData*data);
-OpenGLTable gl;
-// OpenGL Data
-char         g_GlslVersion[32] = "#version 150";
-GLuint       g_FontTexture = 0;
-GLint          g_ShaderHandle = 0, g_VertHandle = 0, g_FragHandle = 0;
-GLint          g_AttribLocationTex = 0, g_AttribLocationProjMtx = 0;
-GLint          g_AttribLocationPosition = 0, g_AttribLocationUV = 0, g_AttribLocationColor = 0;
-GLuint g_VboHandle = 0, g_ElementsHandle = 0;
-
-
-
+    OpenGLTable gl;
+    // OpenGL Data
+    char         g_GlslVersion[32] = "#version 150";
+    GLuint       g_FontTexture = 0;
+    GLint          g_ShaderHandle = 0, g_VertHandle = 0, g_FragHandle = 0;
+    GLint          g_AttribLocationTex = 0, g_AttribLocationProjMtx = 0;
+    GLint          g_AttribLocationPosition = 0, g_AttribLocationUV = 0, g_AttribLocationColor = 0;
+    GLuint g_VboHandle = 0, g_ElementsHandle = 0;
 };
 
 void ImguiOpenGLImpl::render(ImDrawData*draw_data){
@@ -166,82 +125,97 @@ void ImguiOpenGLImpl::render(ImDrawData*draw_data){
 }
 
 
-ImguiOpenGLImpl::ImguiOpenGLImpl(char const*version){
+ImguiOpenGLImpl::ImguiOpenGLImpl(char const*glsl_version){
   // Store GLSL version string so we can refer to it later in case we recreate shaders. Note: GLSL version is NOT the same as GL version. Leave this to NULL if unsure.
   if (glsl_version == NULL)
       glsl_version = "#version 150";
   IM_ASSERT((int)strlen(glsl_version) + 2 < IM_ARRAYSIZE(g_GlslVersion));
   strcpy(g_GlslVersion, glsl_version);
   strcat(g_GlslVersion, "\n");
-  return true;
 
 
   if (!g_FontTexture)
     ImGui_ImplOpenGL3_CreateDeviceObjects();
 
-    // Backup GL state
-    GLint last_texture, last_array_buffer, last_vertex_array;
-    gl.glGetIntegerv(GL_TEXTURE_BINDING_2D, &last_texture);
-    gl.glGetIntegerv(GL_ARRAY_BUFFER_BINDING, &last_array_buffer);
-    gl.glGetIntegerv(GL_VERTEX_ARRAY_BINDING, &last_vertex_array);
+  // Backup GL state
+  GLint last_texture, last_array_buffer, last_vertex_array;
+  gl.glGetIntegerv(GL_TEXTURE_BINDING_2D, &last_texture);
+  gl.glGetIntegerv(GL_ARRAY_BUFFER_BINDING, &last_array_buffer);
+  gl.glGetIntegerv(GL_VERTEX_ARRAY_BINDING, &last_vertex_array);
 
-    // Create shaders
-    const GLchar *vertex_shader =
-        "uniform mat4 ProjMtx;\n"
-        "in vec2 Position;\n"
-        "in vec2 UV;\n"
-        "in vec4 Color;\n"
-        "out vec2 Frag_UV;\n"
-        "out vec4 Frag_Color;\n"
-        "void main()\n"
-        "{\n"
-        "	Frag_UV = UV;\n"
-        "	Frag_Color = Color;\n"
-        "	gl_Position = ProjMtx * vec4(Position.xy,0,1);\n"
-        "}\n";
+  // Create shaders
+  const GLchar *vertex_shader =
+      "uniform mat4 ProjMtx;\n"
+      "in vec2 Position;\n"
+      "in vec2 UV;\n"
+      "in vec4 Color;\n"
+      "out vec2 Frag_UV;\n"
+      "out vec4 Frag_Color;\n"
+      "void main()\n"
+      "{\n"
+      "	Frag_UV = UV;\n"
+      "	Frag_Color = Color;\n"
+      "	gl_Position = ProjMtx * vec4(Position.xy,0,1);\n"
+      "}\n";
 
-    const GLchar* fragment_shader =
-        "uniform sampler2D Texture;\n"
-        "in vec2 Frag_UV;\n"
-        "in vec4 Frag_Color;\n"
-        "out vec4 Out_Color;\n"
-        "void main()\n"
-        "{\n"
-        "	Out_Color = Frag_Color * texture( Texture, Frag_UV.st);\n"
-        "}\n";
+  const GLchar* fragment_shader =
+      "uniform sampler2D Texture;\n"
+      "in vec2 Frag_UV;\n"
+      "in vec4 Frag_Color;\n"
+      "out vec4 Out_Color;\n"
+      "void main()\n"
+      "{\n"
+      "	Out_Color = Frag_Color * texture( Texture, Frag_UV.st);\n"
+      "}\n";
 
-    const GLchar* vertex_shader_with_version[2] = { g_GlslVersion, vertex_shader };
-    const GLchar* fragment_shader_with_version[2] = { g_GlslVersion, fragment_shader };
+  const GLchar* vertex_shader_with_version[2] = { g_GlslVersion, vertex_shader };
+  const GLchar* fragment_shader_with_version[2] = { g_GlslVersion, fragment_shader };
 
-    g_ShaderHandle = gl.glCreateProgram();
-    g_VertHandle = gl.glCreateShader(GL_VERTEX_SHADER);
-    g_FragHandle = gl.glCreateShader(GL_FRAGMENT_SHADER);
-    gl.glShaderSource(g_VertHandle, 2, vertex_shader_with_version, NULL);
-    gl.glShaderSource(g_FragHandle, 2, fragment_shader_with_version, NULL);
-    gl.glCompileShader(g_VertHandle);
-    gl.glCompileShader(g_FragHandle);
-    gl.glAttachShader(g_ShaderHandle, g_VertHandle);
-    gl.glAttachShader(g_ShaderHandle, g_FragHandle);
-    gl.glLinkProgram(g_ShaderHandle);
+  g_ShaderHandle = gl.glCreateProgram();
+  g_VertHandle = gl.glCreateShader(GL_VERTEX_SHADER);
+  g_FragHandle = gl.glCreateShader(GL_FRAGMENT_SHADER);
+  gl.glShaderSource(g_VertHandle, 2, vertex_shader_with_version, NULL);
+  gl.glShaderSource(g_FragHandle, 2, fragment_shader_with_version, NULL);
+  gl.glCompileShader(g_VertHandle);
+  gl.glCompileShader(g_FragHandle);
+  gl.glAttachShader(g_ShaderHandle, g_VertHandle);
+  gl.glAttachShader(g_ShaderHandle, g_FragHandle);
+  gl.glLinkProgram(g_ShaderHandle);
 
-    g_AttribLocationTex = gl.glGetUniformLocation(g_ShaderHandle, "Texture");
-    g_AttribLocationProjMtx = gl.glGetUniformLocation(g_ShaderHandle, "ProjMtx");
-    g_AttribLocationPosition = gl.glGetAttribLocation(g_ShaderHandle, "Position");
-    g_AttribLocationUV = gl.glGetAttribLocation(g_ShaderHandle, "UV");
-    g_AttribLocationColor = gl.glGetAttribLocation(g_ShaderHandle, "Color");
+  g_AttribLocationTex = gl.glGetUniformLocation(g_ShaderHandle, "Texture");
+  g_AttribLocationProjMtx = gl.glGetUniformLocation(g_ShaderHandle, "ProjMtx");
+  g_AttribLocationPosition = gl.glGetAttribLocation(g_ShaderHandle, "Position");
+  g_AttribLocationUV = gl.glGetAttribLocation(g_ShaderHandle, "UV");
+  g_AttribLocationColor = gl.glGetAttribLocation(g_ShaderHandle, "Color");
 
-    gl.glGenBuffers(1, &g_VboHandle);
-    gl.glGenBuffers(1, &g_ElementsHandle);
+  gl.glGenBuffers(1, &g_VboHandle);
+  gl.glGenBuffers(1, &g_ElementsHandle);
 
-    ImGui_ImplOpenGL3_CreateFontsTexture();
+  // Build texture atlas
+  ImGuiIO& io = ImGui::GetIO();
+  unsigned char* pixels;
+  int width, height;
+  io.Fonts->GetTexDataAsRGBA32(&pixels, &width, &height);   // Load as RGBA 32-bits (75% of the memory is wasted, but default font is so small) because it is more likely to be compatible with user's existing shaders. If your ImTextureId represent a higher-level concept than just a GL texture id, consider calling GetTexDataAsAlpha8() instead to save on GPU memory.
 
-    // Restore modified GL state
-    gl.glBindTexture(GL_TEXTURE_2D, last_texture);
-    gl.glBindBuffer(GL_ARRAY_BUFFER, last_array_buffer);
-    gl.glBindVertexArray(last_vertex_array);
+  // Upload texture to graphics system
+  gl.glGetIntegerv(GL_TEXTURE_BINDING_2D, &last_texture);
+  gl.glGenTextures(1, &g_FontTexture);
+  gl.glBindTexture(GL_TEXTURE_2D, g_FontTexture);
+  gl.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  gl.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  gl.glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
+  gl.glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
 
-    return true;
+  // Store our identifier
+  io.Fonts->TexID = (void *)(intptr_t)g_FontTexture;
 
+  // Restore state
+  gl.glBindTexture(GL_TEXTURE_2D, last_texture);
+
+  // Restore modified GL state
+  gl.glBindTexture(GL_TEXTURE_2D, last_texture);
+  gl.glBindBuffer(GL_ARRAY_BUFFER, last_array_buffer);
+  gl.glBindVertexArray(last_vertex_array);
 
 }
 
