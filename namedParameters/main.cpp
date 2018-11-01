091e76c585be990ff9ff1fcacc1617035a653678
Tomáš Milet <amillhouse@seznam.cz>
2018-11-01 15:22:31 +0100

    Named parameters

--- namedParameters/main.cpp
+++ namedParameters/main.cpp
@@ -9,6 +9,80 @@ int func(int a,int b){
   return a+b;
 }
 
+class FuncAB{
+  public:
+    FuncAB(int const&a,int const&b):aa(a),bb(b){}
+    int operator()(){
+      return func(aa,bb);
+    }
+  protected:
+    int const&aa;
+    int const&bb;
+};
+
+class FuncA{
+  public:
+    FuncA(int const&a):aa(a){}
+    FuncAB setB(int const&b){
+      return FuncAB{aa,b};
+    }
+    int operator()(){
+      return func(aa,0);
+    }
+  protected:
+    int const&aa;
+};
+
+class FuncB{
+  public:
+    FuncB(int const&b):bb(b){}
+    FuncAB setA(int const&a){
+      return FuncAB{a,bb};
+    }
+    int operator()(){
+      return func(0,bb);
+    }
+  protected:
+    int const&bb;
+};
+
+class Func{
+  public:
+    FuncA setA(int const&a){
+      return FuncA{a};
+    }
+    FuncB setB(int const&b){
+      return FuncB{b};
+    }
+    int operator()(){
+      return func(0,0);
+    }
+  protected:
+};
+
+template<typename T,typename...ARGS>
+using FCE = void(*)(T&,ARGS const&...);
+
+template<typename T,typename...ARGS>
+FCE<T,ARGS...> getCall(T(*f)(ARGS...)){
+  return [&](T&r,ARGS const&...args){r=f(args...);};
+}
+
+
+int main(){
+  auto f = getCall(func);
+  int r=0,a=12,b=32;
+  f(r,a,b);
+  std::cerr << Func{}                    () << std::endl;
+  std::cerr << Func{}.setA(23 )          () << std::endl;
+  std::cerr << Func{}.setB(200)          () << std::endl;
+  std::cerr << Func{}.setA(23 ).setB(200)() << std::endl;
+  std::cerr << Func{}.setB(200).setA(23 )() << std::endl;
+
+  return 0;
+};
+
+/*
 template<typename T>
 class Ref{
   public:
@@ -61,7 +135,10 @@ class Func{
     int bb;
 };
 
+
 int main(){
   Func().setA(10).setB(9)();
   return 0;
 }
+
+*/
