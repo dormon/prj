7176a2ade81cb74da1c7e21b5b569afa00dd29eb
dormon <imilet@fit.vutbr.cz>
2020-08-21 02:10:07 +0200

    open dialog

--- remsub/mainMenu.cpp
+++ remsub/mainMenu.cpp
@@ -2,11 +2,13 @@
 #include <Vars/Vars.h>
 #include <imguiDormon/imgui.h>
 #include <Project.hpp>
+#include <ImGuiFileDialog/ImGuiFileDialog.h>
 
 void mainMenu(vars::Vars&vars){
   if(ImGui::BeginMainMenuBar()){
     if(ImGui::BeginMenu("file")){
       if(ImGui::MenuItem("open")){
+        igfd::ImGuiFileDialog::Instance()->OpenDialog("ChooseFileDlgKey", "Choose File", ".cpp,.h,.hpp", ".");
       }
       if(ImGui::MenuItem("save")){
         vars.get<Project>("project")->save(vars.getString("projectName"));
@@ -14,5 +16,19 @@ void mainMenu(vars::Vars&vars){
       ImGui::EndMenu();
     }
     ImGui::EndMainMenuBar();
+  }
+
+  // display
+  if (igfd::ImGuiFileDialog::Instance()->FileDialog("ChooseFileDlgKey")) 
+  {
+    // action if OK
+    if (igfd::ImGuiFileDialog::Instance()->IsOk == true)
+    {
+      std::string filePathName = igfd::ImGuiFileDialog::Instance()->GetFilePathName();
+      std::string filePath = igfd::ImGuiFileDialog::Instance()->GetCurrentPath();
+      // action
+    }
+    // close
+    igfd::ImGuiFileDialog::Instance()->CloseDialog("ChooseFileDlgKey");
   }
 }
